d5d101c1749a0b139b99949b95c47706bed758d1
seriskey <zihlingchen@gmail.com>
2025-04-06 17:23:04 +0800

    node-training-postgresql week4 作業

--- week4/db.js
+++ week4/db.js
@@ -10,7 +10,52 @@ const CreditPackage = new EntitySchema({
       generated: "uuid",
       nullable: false,
     },
+    name: {
+      type: "varchar",
+      length: 50,
+      nullable: false,
+      unique: true,
+    },
+    credit_amount: {
+      type: "integer",
+      nullable: false,
+    },
+    price: {
+      type: "numeric",
+      precision: 10,
+      scale: 2,
+      nullable: false
+    },
+    created_at: {
+      type: "timestamp",
+      nullable: false,
+      createDate:true,
+    }
   },
+})
+
+const Skill = new EntitySchema({
+  name: "Skill",
+  tableName: "SKILL",
+  columns: {
+    id: {
+      primary: true,
+      type: "uuid",
+      generated: "uuid",
+      nullable: false,
+    },
+    name: {
+      type:"varchar",
+      length: 50,
+      nullable: false,
+      unique: true
+    },
+    created_at: {
+      type: "timestamp",
+      nullable: false,
+      createDate:true,
+    }
+  }
 })
 
 const AppDataSource = new DataSource({
@@ -20,7 +65,7 @@ const AppDataSource = new DataSource({
   username: process.env.DB_USERNAME || "root",
   password: process.env.DB_PASSWORD || "test",
   database: process.env.DB_DATABASE || "test",
-  entities: [CreditPackage],
+  entities: [CreditPackage, Skill],
   synchronize: true,
 })
 
